ebb601fe8cc3cf3fb70241d7de39eb78edef5ee4
Dave Dash <dd+github@davedash.com>
2011-07-29 11:52:14 -0700

    Enable intersphinx

--- conf.py
+++ conf.py
@@ -25,7 +25,7 @@ import sys, os
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = []
+extensions = ['sphinx.ext.intersphinx']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -214,3 +214,7 @@ man_pages = [
     ('index', 'webdevbootcamp', u'Webdev Bootcamp Documentation',
      [u'Dave Dash, Laura Thompson, Jeff Balogh, Mozilla Webdev Team'], 1)
 ]
+
+intersphinx_mapping = dict(
+    playdoh=('http://playdoh.readthedocs.org/en/latest/', None)
+    )
